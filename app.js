3302c5ed60f5377ef8b92eaf47aa4eea89d2341c
刘超凡 <lcfme@sina.com>
2017-12-13 19:44:51 +0800

    done

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
-server.connection({ port: process.env.PORT || 80, host: 'localhost' });
 
-server.register(require('inert'), (err) => {
+server.connection({
+  host: 'localhost',
+  port: process.env.PORT || 3000,
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    reply('Hello World from Hapi Server');
+  },
+});
+
+server.start((err) => {
   if (err) {
     throw err;
   }
-  // Serving Static Content
-  server.route({
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-      directory: {
-        path: 'public',
-        index: ['index.html'],
-        listing: true,
-      },
-    },
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, reply) => {
-      reply('Hi, Syy');
-    },
-  });
-
   // eslint-disable-next-line
-  server.start(err => {
-    if (err) {
-      throw err;
-    }
-    // eslint-disable-next-line
-    console.log('Server running at:', server.info.uri);
-  });
+  console.log(`Server running at ${server.info.uri}`);
 });
